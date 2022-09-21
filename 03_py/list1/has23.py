6733f46d4ec9a74a08f243c638de754a5d78e64c
samsonahh <swu30@stuy.edu>
2022-09-21 18:54:32 -0400

    Finished adding comments, solutions, and tests

--- 03_py/list1/has23.py
+++ 03_py/list1/has23.py
@@ -1,2 +1,12 @@
 def has23(nums):
-  return nums[0] == 2 or nums[1] == 3 or nums[0] == 3 or nums[1] == 2
+  return nums[0] == 2 or nums[0] == 3 or nums[1] == 2 or nums[1] == 3 #Checks for each case for a list of 2 ints if it has a 2 or a 3
+
+print(has23([2, 5])) # → True
+print(has23([4, 3])) # → True
+print(has23([4, 5])) # → False
+print(has23([2, 2])) # → True
+print(has23([3, 2])) # → True
+print(has23([3, 3])) # → True
+print(has23([7, 7])) # → False
+print(has23([3, 9])) # → True
+print(has23([9, 5])) # → False
