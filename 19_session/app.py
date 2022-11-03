81ec5b27621ef75279a89b82e72a713b0454385a
samsonahh <swu30@stuy.edu>
2022-11-03 14:24:45 -0400

    Test

--- 19_session/app.py
+++ 19_session/app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request, session
 app = Flask(__name__)
 app.secret_key = b'_MinecraftSTEVE'
 
+
 @app.route('/')
 def index():
     if 'username' in session:
