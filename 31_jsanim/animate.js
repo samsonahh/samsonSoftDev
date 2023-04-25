ba86b20650f3004227e1ddb7be309196d3878033
Samson Wu <swu30@stuy.edu>
2023-04-25 13:18:12 -0400

    finished animation

--- 31_jsanim/animate.js
+++ 31_jsanim/animate.js
@@ -1,5 +1,54 @@
+// Samson Wu, Ravindra Mangar pd7
+
+var c = document.getElementById("playground");
 var animButton = document.getElementById("buttonCircle");
 var stopButton = document.getElementById("buttonStop");
 
-animButton.addEventListener("click", ()=>{console.log("ANIMATE!");})
-stopButton.addEventListener("click", ()=>{console.log("STOP!");})
+var ctx = c.getContext("2d");
+
+ctx.fillStyle = "green";
+
+var requestID;
+
+var clear = () => {
+    ctx.clearRect(0, 0, c.width, c.height);
+}
+
+var radius = 0;
+var growing = true;
+
+var drawCircle = () => {
+    //console.log("DRAWING!");
+    clear();
+
+    ctx.beginPath();
+    ctx.arc(c.width/2, c.height/2, radius, 0, 2*Math.PI);
+    ctx.fill();
+    ctx.closePath();
+
+    if(growing){
+        radius++;
+    }
+    else{
+        radius--;
+    }
+
+    if(radius == c.width/2){
+        growing = false;
+    }
+    if(radius == 0){
+        growing = true;
+    }
+
+    window.cancelAnimationFrame(requestID);
+    requestID = window.requestAnimationFrame(drawCircle);
+    console.log(requestID);
+}
+
+var stopAnim = (e) => {
+    window.cancelAnimationFrame(requestID);
+    //console.log("STOPPED", requestID);
+}
+
+animButton.addEventListener("click", drawCircle);
+stopButton.addEventListener("click", stopAnim);
